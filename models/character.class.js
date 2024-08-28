13055e3d727b075ebfad07d67fb2259717aae33e
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-08-28 13:42:10 +0200

    Charakter Movin Left & Right

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MovableObject {
   y = 230;
   width = 100;
   height = 200;
+  speed = 10;
   IMAGES_WALKING = [
     'img/2_character_pepe/2_walk/W-21.png',
     'img/2_character_pepe/2_walk/W-22.png',
@@ -10,6 +11,7 @@ class Character extends MovableObject {
     'img/2_character_pepe/2_walk/W-25.png',
     'img/2_character_pepe/2_walk/W-26.png',
   ];
+  world;
 
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
@@ -19,11 +21,26 @@ class Character extends MovableObject {
 
   animate() {
     setInterval(() => {
-      let i = this.currentImage % this.IMAGES_WALKING.length;
-      let path = this.IMAGES_WALKING[i];
-      this.img = this.imageCache[path];
-      this.currentImage++;
-    }, 100);    
+      if (this.world.keyboard.RIGHT) {
+        this.x += this.speed;        
+      }
+
+      if (this.world.keyboard.LEFT) {
+        this.x -= this.speed;        
+      }
+      
+    }, 1000 / 30);
+
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+
+        // Walk animation
+        let i = this.currentImage % this.IMAGES_WALKING.length;
+        let path = this.IMAGES_WALKING[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
+      }
+    }, 144);   
   }
 
   jump() {}
