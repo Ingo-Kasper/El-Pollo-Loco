2572a758c048d3ad6c69ea1e2b8724e91d2ad4f1
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-08-28 09:16:37 +0200

    Add Sky & fix Charakter

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,11 +1,11 @@
 class BackgroundObject extends MovableObject {
   width = 720;
-  height = 150;
+  height = 480;
 
   constructor(imagePath) {
     super().loadImage(imagePath);
 
     this.x = 0;
-    this.y = 350;
+    this.y = 480 - this.height;
   }
 }
--- models/character.class.js
+++ models/character.class.js
@@ -1,10 +1,11 @@
 class Character extends MovableObject {
+
+  y = 130;
+  width = 150;
+  height = 300;
+
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
-
-    this.y = 250;
-    this.width = 100;
-    this.height = 200;
   }
 
   jump() {}
