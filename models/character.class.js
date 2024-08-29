30f1efaf51f8ea0e270b4ac4c480963d5e50b498
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-08-29 08:58:18 +0200

    add Charakter Gravitazon & Jump Animation

--- models/character.class.js
+++ models/character.class.js
@@ -1,35 +1,50 @@
 class Character extends MovableObject {
-  y = 230;
+  y = 30;
   width = 100;
   height = 200;
   speed = 10;
   IMAGES_WALKING = [
-    'img/2_character_pepe/2_walk/W-21.png',
-    'img/2_character_pepe/2_walk/W-22.png',
-    'img/2_character_pepe/2_walk/W-23.png',
-    'img/2_character_pepe/2_walk/W-24.png',
-    'img/2_character_pepe/2_walk/W-25.png',
-    'img/2_character_pepe/2_walk/W-26.png',
+    "img/2_character_pepe/2_walk/W-21.png",
+    "img/2_character_pepe/2_walk/W-22.png",
+    "img/2_character_pepe/2_walk/W-23.png",
+    "img/2_character_pepe/2_walk/W-24.png",
+    "img/2_character_pepe/2_walk/W-25.png",
+    "img/2_character_pepe/2_walk/W-26.png",
+  ];
+  IMAGES_JUMPING = [
+    "img/2_character_pepe/3_jump/J-31.png",
+    "img/2_character_pepe/3_jump/J-32.png",
+    "img/2_character_pepe/3_jump/J-34.png",
+    "img/2_character_pepe/3_jump/J-34.png",
+    "img/2_character_pepe/3_jump/J-35.png",
+    "img/2_character_pepe/3_jump/J-36.png",
+    "img/2_character_pepe/3_jump/J-37.png",
+    "img/2_character_pepe/3_jump/J-38.png",
+    "img/2_character_pepe/3_jump/J-39.png",
   ];
   world;
-  walking_sound = new Audio('../audio/el-Pollo-Koco-Classig/running.mp3')
+  walking_sound = new Audio("../audio/el-Pollo-Koco-Classig/running.mp3");
 
   constructor() {
     super().loadImage("img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_JUMPING);
     this.animate();
+    this.applyGravity();
   }
 
   animate() {
     setInterval(() => {
       this.walking_sound.pause();
-      if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) { // after the && the movement to the right is stopped
+      if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
+        // after the && the movement to the right is stopped
         this.x += this.speed;
         this.otherDirection = false;
         this.walking_sound.play();
       }
 
-      if (this.world.keyboard.LEFT && this.x > -500) { // after the && the movement to the left is stopped
+      if (this.world.keyboard.LEFT && this.x > -500) {
+        // after the && the movement to the left is stopped
         this.x -= this.speed;
         this.otherDirection = true;
         this.walking_sound.play();
@@ -38,15 +53,16 @@ class Character extends MovableObject {
     }, 1000 / 25);
 
     setInterval(() => {
-      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+      // Die Animation werden geändert
+      if (this.isAboveGround()) {
+        this.playAnimation(this.IMAGES_JUMPING); // Jumpung animation
+      } else {
 
-        // Walk animation
-        let i = this.currentImage % this.IMAGES_WALKING.length;
-        let path = this.IMAGES_WALKING[i];
-        this.img = this.imageCache[path];
-        this.currentImage++;
+        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+          this.playAnimation(this.IMAGES_WALKING); // Walking animation
+        }
       }
-    }, 110);   
+    }, 110);
   }
 
   jump() {}
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -8,6 +8,22 @@ class MovableObject {
   imageCache = {};
   currentImage = 0;
   otherDirection = false;
+  speedY = 0;
+  acceleration = 2;
+
+  applyGravity() {
+    // Schwergraft
+    setInterval(() => {
+      if (this.isAboveGround()) {
+        this.y -= this.speedY;
+        this.speedY -= this.acceleration;
+      }
+    }, 1000 / 25);
+  }
+
+  isAboveGround() {
+    return this.y < 230;
+  }
 
   loadImage(path) {
     this.img = new Image();
