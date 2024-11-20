acf4af6c1f279877867747dbe30df238dd4c749c
Ingo-Kasper <Djindustrie@Protonmail.com>
2024-11-20 06:39:44 +0100

    background-object.class, Funktionen sind nach JSDoc Standard dokumentiert

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -2,6 +2,11 @@ class BackgroundObject extends MovableObject {
   width = 720;
   height = 480;
 
+/**
+* Constructor for the background object
+* @param {string} imagePath The path to the background object's image
+* @param {number} x The x-position of the background object
+*/
   constructor(imagePath, x) {
     super();
     this.loadImage(imagePath)
